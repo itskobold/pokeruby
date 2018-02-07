7a975b59170bd8f30a525915051c7385f47c1a26
itskobold <itskobold@users.noreply.github.com>
2018-02-07 08:21:19 +0000

    goode
    weeeeee

--- hoenn isles/Documents/keyboard layouts/vanilla keyboards.c
+++ hoenn isles/Documents/keyboard layouts/vanilla keyboards.c
@@ -1,24 +1,24 @@
 //for src/engine/naming_screen.c
 //multiple types of keyboard layouts that can be changed in the options menu
 
-//RUBY upper page
+//VANILLA upper page
 //same as vanilla
     {
         _(" A B C  D E F    . "),
-        _(" G H I  J K L    , "),
+        _(" G H I  J K L É  , "),
         _(" M N O  P Q R S    "),
         _(" T U V  W X Y Z    "),
     },
 	
-//RUBY lower page
+//VANILLA lower page
     {
         _(" a b c  d e f    . "),
-        _(" g h i  j k l    , "),
+        _(" g h i  j k l é  , "),
         _(" m n o  p q r s    "),
         _(" t u v  w x y z    "),
     },
 	
-//RUBY others page
+//VANILLA others page
     {
         _(" 0  1  2  3  4     "),
         _(" 5  6  7  8  9     "),
